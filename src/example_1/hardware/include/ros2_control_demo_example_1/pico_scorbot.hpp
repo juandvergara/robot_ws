57d9727ec0f9b0a23c951ba060f95101ff32a11a
juandvergara <juandavidelp99@gmail.com>
2023-04-30 19:24:07 -0500

    Add sendData to serial comms

--- src/example_1/hardware/include/ros2_control_demo_example_1/pico_scorbot.hpp
+++ src/example_1/hardware/include/ros2_control_demo_example_1/pico_scorbot.hpp
@@ -3,27 +3,37 @@
 
 #include <sstream>
 #include <libserial/SerialPort.h>
+#include <libserial/SerialStream.h>
 #include <iostream>
-
 
 LibSerial::BaudRate convert_baud_rate(int baud_rate)
 {
   // Just handle some common baud rates
   switch (baud_rate)
   {
-    case 1200: return LibSerial::BaudRate::BAUD_1200;
-    case 1800: return LibSerial::BaudRate::BAUD_1800;
-    case 2400: return LibSerial::BaudRate::BAUD_2400;
-    case 4800: return LibSerial::BaudRate::BAUD_4800;
-    case 9600: return LibSerial::BaudRate::BAUD_9600;
-    case 19200: return LibSerial::BaudRate::BAUD_19200;
-    case 38400: return LibSerial::BaudRate::BAUD_38400;
-    case 57600: return LibSerial::BaudRate::BAUD_57600;
-    case 115200: return LibSerial::BaudRate::BAUD_115200;
-    case 230400: return LibSerial::BaudRate::BAUD_230400;
-    default:
-      std::cout << "Error! Baud rate " << baud_rate << " not supported! Default to 57600" << std::endl;
-      return LibSerial::BaudRate::BAUD_57600;
+  case 1200:
+    return LibSerial::BaudRate::BAUD_1200;
+  case 1800:
+    return LibSerial::BaudRate::BAUD_1800;
+  case 2400:
+    return LibSerial::BaudRate::BAUD_2400;
+  case 4800:
+    return LibSerial::BaudRate::BAUD_4800;
+  case 9600:
+    return LibSerial::BaudRate::BAUD_9600;
+  case 19200:
+    return LibSerial::BaudRate::BAUD_19200;
+  case 38400:
+    return LibSerial::BaudRate::BAUD_38400;
+  case 57600:
+    return LibSerial::BaudRate::BAUD_57600;
+  case 115200:
+    return LibSerial::BaudRate::BAUD_115200;
+  case 230400:
+    return LibSerial::BaudRate::BAUD_230400;
+  default:
+    std::cout << "Error! Baud rate " << baud_rate << " not supported! Default to 57600" << std::endl;
+    return LibSerial::BaudRate::BAUD_57600;
   }
 }
 
@@ -31,86 +41,74 @@ class PicoScorbot
 {
 
 public:
-
   PicoScorbot() = default;
 
-  void connect(const std::string &serial_device, int32_t baud_rate, int32_t timeout_ms)
-  {  
+  void connect(const std::string &serial_upper_device, const std::string &serial_lower_device, int32_t baud_rate, int32_t timeout_ms)
+  {
     timeout_ms_ = timeout_ms;
-    serial_conn_.Open(serial_device);
-    serial_conn_.SetBaudRate(convert_baud_rate(baud_rate));
+    serial_upper_conn_.Open(serial_upper_device);
+    serial_upper_conn_.SetBaudRate(convert_baud_rate(baud_rate));
+
+    serial_lower_conn_.Open(serial_lower_device);
+    serial_lower_conn_.SetBaudRate(convert_baud_rate(baud_rate));
   }
 
   void disconnect()
   {
-    serial_conn_.Close();
+    serial_upper_conn_.Close();
+    serial_lower_conn_.Close();
   }
 
   bool connected() const
   {
-    return serial_conn_.IsOpen();
+    return serial_upper_conn_.IsOpen() && serial_lower_conn_.IsOpen();
   }
 
-
-  std::string send_msg(const std::string &msg_to_send, bool print_output = false)
+  void sendDataToDevices(std::vector<double> &data)
   {
-    serial_conn_.FlushIOBuffers(); // Just in case
-    serial_conn_.Write(msg_to_send);
-
-    std::string response = "";
-    try
+    if (data.size() != 7)
     {
-      // Responses end with \r\n so we will read up to (and including) the \n.
-      serial_conn_.ReadLine(response, '\n', timeout_ms_);
+      std::cout << "Error: Input data must be a vector of size 7." << std::endl;
+      return;
     }
-    catch (const LibSerial::ReadTimeout&)
+    std::string position_lower = "p " + std::to_string((data)[0]) + "," + std::to_string((data)[1]) + "," + std::to_string((data)[2]);
+    std::string position_upper = "p " + std::to_string((data)[3]) + "," + std::to_string((data)[4]) + "," + std::to_string((data)[5]);
+    std::string position_extruder = "n " + std::to_string((data)[6]);
+
+    serial_lower_conn_.Write(position_lower + "\n");
+    serial_upper_conn_.Write(position_upper + "\n");
+    serial_upper_conn_.Write(position_extruder + "\n");
+  }
+
+  void getDataFromDevices(std::vector<double> &data)
+  {
+    std::string response;
+    std::string delimiter = ",";
+
+    serial_lower_conn_.Write("e\n");
+    serial_lower_conn_.ReadLine(response, '\n', timeout_ms_);
+
+    while (response.find(delimiter) != std::string::npos)
     {
-        std::cerr << "The ReadByte() call has timed out." << std::endl ;
+      std::string dato = response.substr(0, response.find(','));
+      actual_pos_joints.push_back(std::atof(dato.c_str()));
+      response.erase(0, response.find(',') + 1);
     }
 
-    if (print_output)
+    for (size_t i = 0; i < actual_pos_joints.size(); i++)
     {
-      std::cout << "Sent: " << msg_to_send << " Recv: " << response << std::endl;
+      data[i] = actual_pos_joints[i];
     }
 
-    return response;
-  }
+    serial_upper_conn_.Write("e\n");
+    serial_upper_conn_.ReadLine(response, '\n', timeout_ms_);
 
-
-  void send_empty_msg()
-  {
-    std::string response = send_msg("\r");
-  }
-
-  void read_encoder_values(int &val_1, int &val_2)
-  {
-    std::string response = send_msg("e\r");
-
-    std::string delimiter = " ";
-    size_t del_pos = response.find(delimiter);
-    std::string token_1 = response.substr(0, del_pos);
-    std::string token_2 = response.substr(del_pos + delimiter.length());
-
-    val_1 = std::atoi(token_1.c_str());
-    val_2 = std::atoi(token_2.c_str());
-  }
-  void set_motor_values(int val_1, int val_2)
-  {
-    std::stringstream ss;
-    ss << "m " << val_1 << " " << val_2 << "\r";
-    send_msg(ss.str());
-  }
-
-  void set_pid_values(int k_p, int k_d, int k_i, int k_o)
-  {
-    std::stringstream ss;
-    ss << "u " << k_p << ":" << k_d << ":" << k_i << ":" << k_o << "\r";
-    send_msg(ss.str());
   }
 
 private:
-    LibSerial::SerialPort serial_conn_;
-    int timeout_ms_;
+  LibSerial::SerialPort serial_lower_conn_, serial_upper_conn_;
+  std::vector<double> actual_pos_joints, actual_vel_joints;
+  int timeout_ms_;
 };
 
 #endif // SCORBOT_PICO_COMMS_HPP
